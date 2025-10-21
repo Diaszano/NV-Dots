1fa1755a4aeacce37c3b3218389f8b4215ca7a30
Diaszano <61257292+Diaszano@users.noreply.github.com>
2025-10-21 03:39:54 -0300

    feat: add Mason integration for linting and tool installation

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,21 @@
 return {
   {
+    "mason-org/mason.nvim",
+    opts = require("configs.mason"),
+  },
+  {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre",
     opts = require("configs.conform"),
   },
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPost", "BufNewFile", "BufWritePost", "InsertLeave" },
+    config = function()
+      require("configs.lint")
+    end,
+  },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -13,9 +23,36 @@ return {
     end,
   },
 
+  {
+    "rshkarin/mason-nvim-lint",
+    config = function()
+      require("configs.mason.lint")
+    end,
+    dependencies = {
+      "mason-org/mason.nvim",
+      "mfussenegger/nvim-lint",
+    },
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = function()
+      require("configs.mason.tool")
+    end,
+    dependencies = {
+      "mason-org/mason.nvim",
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = require("configs.mason.lspconfig"),
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+  },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
-
   {
     "nvim-treesitter/nvim-treesitter",
     opts = require("configs.treesitter"),
